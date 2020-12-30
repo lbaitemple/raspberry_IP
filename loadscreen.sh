c2212a4befd370fe8a4918c0641979aacfb066bd
lbaitemple <lbai@temple.edu>
2020-12-30 09:13:20 -0500

    Update loadscreen.sh

--- loadscreen.sh
+++ loadscreen.sh
@@ -2,7 +2,7 @@ sudo apt-get update
 sudo apt-get install python3-pip python3-pil  i2c-tools git mosquitto-clients -y
 git clone -b jupyter https://github.com/lbaitemple/raspberry_IP/
 sudo pip3 install --system  Adafruit_SSD1306 RPi.GPIO
-sudo sh raspberry_IP/setup-i2c.sh
+sudo bash raspberry_IP/setup-i2c.sh
 cp raspberry_IP/newtest2.sh ~/test2.sh
 cp raspberry_IP/stats.py ~/stats.py
 chmod +x ~/test2.sh
