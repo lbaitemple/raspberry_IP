ec2f39267c414ebd5a6e8dfe50f6c7201834b361
lbaitemple <lbai@temple.edu>
2020-12-31 16:22:39 -0500

    Update loadscreen.sh
    add more swap memory

--- loadscreen.sh
+++ loadscreen.sh
@@ -11,3 +11,8 @@ sudo cp raspberry_IP/ipaddress.service /lib/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable  ipaddress
 sudo systemctl start  ipaddress
+
+sudo dd if=/dev/zero of=/swap1 bs=1M count=4096
+sudo mkswap /swap1
+sudo swapon /swap1
+echo "/swap1 swap swap" >> /etc/fstab
