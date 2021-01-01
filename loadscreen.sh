19bcc7df0b3b6654a38ca9ca1afc52b1a00a7a8f
lbaitemple <lbai@temple.edu>
2021-01-01 00:12:50 -0500

    Update loadscreen.sh

--- loadscreen.sh
+++ loadscreen.sh
@@ -15,4 +15,4 @@ sudo systemctl start  ipaddress
 sudo dd if=/dev/zero of=/swap1 bs=1M count=4096
 sudo mkswap /swap1
 sudo swapon /swap1
-echo "/swap1 swap swap" >> /etc/fstab
+sudo bash -c "echo '/swap1 swap swap' >> /etc/fstab"
