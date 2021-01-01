8395e31076fa3f52665b86cd04128f07a3df5349
lbaitemple <lbai@temple.edu>
2020-12-31 22:25:50 -0500

    Update installros.sh

--- installros.sh
+++ installros.sh
@@ -2,7 +2,7 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 sudo apt install -y python-rosdep python-rosinstall-generator python-wstool python-rosinstall build-essential cmake
 sudo rosdep init
 rosdep update
