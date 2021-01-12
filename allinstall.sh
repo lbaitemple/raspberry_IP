a17103a2a653899d33693e43c9622de035669d02
lbaitemple <lbai@temple.edu>
2021-01-11 19:18:07 -0500

    Update allinstall.sh

--- allinstall.sh
+++ allinstall.sh
@@ -10,6 +10,11 @@ cd ~/raspberry_IP/scripts
 chmod +x jupyter.sh
 ./jupyter.sh
 
+echo "There is a long wait here to create a swap memory ...., do not use ctrl-c to cancel the installation process"
+cd ~/raspberry_IP/
+chmod +x installros.sh
+./installros.sh
+
 cd ~/raspberry_IP/
 chmod +x installros.sh
 ./installros.sh
