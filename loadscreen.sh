e9c074d4547052ff28771962c094f5f1acd2389a
lbaitemple <lbai@temple.edu>
2021-01-12 10:49:42 -0500

    Update loadscreen.sh

--- loadscreen.sh
+++ loadscreen.sh
@@ -2,11 +2,11 @@ sudo apt-get update
 sudo apt-get install python3-pip python3-pil  i2c-tools git mosquitto-clients -y
 sudo pip3 install --system  Adafruit_SSD1306 RPi.GPIO  Adafruit-BBIO
 sudo bash raspberry_IP/setup-i2c.sh
-cp raspberry_IP/newtest2.sh ~/test2.sh
-cp raspberry_IP/stats.py ~/stats.py
+cp ../raspberry_IP/newtest2.sh ~/test2.sh
+cp ../raspberry_IP/stats.py ~/stats.py
 chmod +x ~/test2.sh
 sudo systemctl enable systemd-networkd-wait-online.service
-sudo cp raspberry_IP/ipaddress.service /lib/systemd/system
+sudo cp ../raspberry_IP/ipaddress.service /lib/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable  ipaddress
 sudo systemctl start  ipaddress
