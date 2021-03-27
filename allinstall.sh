c951933a19fc5b84701b28ecafdc330b5e3b26a2
lbaitemple <lbai@temple.edu>
2021-03-26 21:52:54 -0400

    Update allinstall.sh

--- allinstall.sh
+++ allinstall.sh
@@ -2,14 +2,6 @@ cd ~/raspberry_IP/
 chmod +x loadscreen.sh
 ./loadscreen.sh
 
-cd ~/raspberry_IP/
-chmod +x greengrass.sh
-./greengrass.sh
-
-cd ~/raspberry_IP/scripts
-chmod +x jupyter.sh
-./jupyter.sh
-
 echo "There is a long wait here to create a swap memory ...., do not use ctrl-c to cancel the installation process"
 cd ~/raspberry_IP/
 chmod +x addswap.sh
@@ -18,4 +10,14 @@ chmod +x addswap.sh
 cd ~/raspberry_IP/
 chmod +x installros.sh
 ./installros.sh
+
+cd ~/raspberry_IP/
+chmod +x greengrass.sh
+./greengrass.sh
+
+cd ~/raspberry_IP/scripts
+chmod +x jupyter.sh
+./jupyter.sh
+
+
 
