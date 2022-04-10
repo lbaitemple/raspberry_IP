be0f4b329ce5811abed8d53f53772446b1142e80
lbaitemple <lbai@temple.edu>
2022-04-10 18:24:35 -0400

    Update newtest2.sh

--- newtest2.sh
+++ newtest2.sh
@@ -4,18 +4,12 @@
 # Please modify your cloud mqtt credential here
 ##############
 m_i2c="3c"
-m_server="m16.cloudmqtt.com"
-m_port=12247
-m_user="pspniyjc"
-m_pass="sBm4EpaDgRe5"
-m_topic="raspberry/ipaddress"
 
 ################
 # nothing needs to be changed below
 ##############
 echo "Starting script sayIPbs "
 private=`hostname -I | sed -E -e 's/[[:blank:]]+/_/g' `
-res=`mosquitto_pub -h $m_server -p $m_port -u $m_user -P $m_pass -t $m_topic -m $private  > /dev/null 2>&1`
 
 mapfile -t data < <(i2cdetect -y 1)
 
